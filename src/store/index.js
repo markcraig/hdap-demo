b494fc2e5aaf546107e730ee7a916a5f6b52f13b
Mark Craig <mark.craig@gmail.com>
2024-03-10 21:06:36 +0100

    Persist store to maintain authn state across pages

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 // Utilities
 import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 
-export default createPinia()
+export default createPinia().use(createPersistedState({ auto: true }))
