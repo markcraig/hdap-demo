9e7b00df53dd01dfef2c361ee0380028c1dec7e4
Mark Craig <mark.craig@gmail.com>
2024-04-20 09:35:29 +0200

    fixup

---
+++ src/components/Viewer.vue
@@ -0,0 +1,13 @@
+<script>
+import { h } from 'vue'
+import { useView } from '@/helpers/view'
+
+const view = useView()
+
+export default {
+    props: ['schema', 'attribute', 'value'],
+    setup(props) {
+        return () => view.format(props.schema, props.attribute, props.value)
+    }
+}
+</script>
